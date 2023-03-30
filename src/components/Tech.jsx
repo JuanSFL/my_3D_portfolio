72006cf679a8d5d1befc8c2ce61bb89163ca7af0
Juan Molina <juan@Juans-MacBook-Pro.local>
2023-03-30 16:00:13 -0400

    added technologies section that highlights skills with movable ball icons

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,11 +1,30 @@
-import { BallCanvas } from "./canvas"
-import { SectionWrapper } from "../hoc"
-import { technologies } from "../constants"
+import React from "react";
+import { styles } from "../styles"
+
+import { BallCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { technologies } from "../constants";
+import { motion } from "framer-motion"
+import { textVariant } from "../utils/motion"
 
 const Tech = () => {
   return (
-    <div>Tech</div>
-  )
-}
+    <div> 
+      <motion.div variants={textVariant()}>
+      <p className={styles.sectionSubText}>Skills</p>
+        <h2 className={styles.sectionHeadText}>Technologies Used.</h2>
+      </motion.div>
+    <div className='flex flex-row flex-wrap justify-center gap-2'>
+      
+      {technologies.map((technology) => (
+        <div className='w-28 h-28' key={technology.name}>
+          <BallCanvas icon={technology.icon} />
+        </div>
+      ))}
+    </div>
+</div>
 
-export default Tech
+  );
+};
+
+export default SectionWrapper(Tech, "");
